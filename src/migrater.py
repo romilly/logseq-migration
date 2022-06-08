ea3d50165706c9dd379c46577fd3777ced9695cb
Romilly Cocking <romilly.cocking@gmail.com>
2022-06-08 08:56:01 +0100

    Refactored code to reduce complexity, express intent. Next step: e2e tests to improve coverage.

--- src/migrater.py
+++ src/migrater.py
@@ -35,33 +35,54 @@ def get_file_name(url: str):
     return file_name
 
 
-def process(file_path: str, lines: List[Tuple[int, str]], asset_dir: str, relative_asset_dir: str):
+def process_assets(file_path: str, lines: List[Tuple[int, str]], asset_dir: str, relative_asset_dir: str):
+    content = update_markdown_content(asset_dir, file_path, lines, relative_asset_dir)
+    with open(file_path, 'w') as md:
+        md.write(''.join(content))
+
+
+def update_markdown_content(asset_dir, file_path, lines, relative_asset_dir):
     with open(file_path) as md:
         content = md.readlines()
         for number, line in lines:
             link = link_from(line)
             new_location = add_url_to_assets(link, asset_dir, relative_asset_dir)
             content[number] = content[number].replace(link, new_location)
-    with open(file_path, 'w') as md:
-        md.write(''.join(content))
+    return content
 
 
 def migrate(vault_directory):
     relative_assets_dir = 'assets'
     assets_from_page_dir = os.path.join('..', relative_assets_dir)
     assets_dir = os.path.join(vault_directory, relative_assets_dir)
-    if not os.path.exists(assets_dir):
-        os.makedirs(assets_dir)
+    ensure_assets_dir_exists(assets_dir)
+    process_files(assets_dir, assets_from_page_dir, vault_directory)
+
+
+def process_files(assets_dir, assets_from_page_dir, vault_directory):
     for subdir, dirs, files in os.walk(vault_directory):
         for file in files:
-            if file.endswith('.md'):
-                file_path = os.path.join(subdir, file)
-                with open(file_path) as md:
-                    numbered_lines = enumerate(line for line in md)
-                    lines = list((number, line) for
-                                 number, line in numbered_lines if 'https://firebasestorage' in line)
-                if len(lines) > 0:
-                    process(file_path, lines, assets_dir, assets_from_page_dir)
+            process_file(assets_dir, assets_from_page_dir, file, subdir)
+
+
+def ensure_assets_dir_exists(assets_dir):
+    if not os.path.exists(assets_dir):
+        os.makedirs(assets_dir)
+
+
+def process_file(assets_dir, assets_from_page_dir, file, subdir):
+    if file.endswith('.md'):
+        file_path = os.path.join(subdir, file)
+        with open(file_path) as md:
+            numbered_lines = enumerate(line for line in md)
+            lines = find_asset_references(numbered_lines)
+        if len(lines) > 0:
+            process_assets(file_path, lines, assets_dir, assets_from_page_dir)
+
+
+def find_asset_references(numbered_lines):
+    return list((number, line) for
+                number, line in numbered_lines if 'https://firebasestorage' in line)
 
 
 if __name__ == '__main__':
