749b667b7ef489585b5cd1f5dff31d0b4da42f17
Romilly Cocking <romilly.cocking@gmail.com>
2022-06-10 15:01:37 +0100

    Added end-to-end test

--- ROADMAP.md
+++ ROADMAP.md
@@ -4,13 +4,14 @@ The next steps are
 
 1. Invite _beta-testers_.
 2. Write an end-to-end test.
-   1. Create a test graph in roam,
-      1. with four linked nodes
-      2. and a pdf. a png, an mp4 and an mp3
-   2. export json,
-   3. add it to this project
-   4. import using the instructions on the logseq hub
-   5. copy the un-modified imported graph as test data in this project.
+   1. ~~Create a test graph in roam,~~
+      1. ~~with four linked nodes~~
+      2. ~~and a pdf. a png and an mp4~~
+   2. ~~export json~~
+   3. ~~add it to this project~~
+   4. ~~import using the instructions on the logseq hub~~
+      1. pdf ref does not import. I suspect the format has changed.
+   5. ~~copy the un-modified imported graph directory as test data in this project.~~
    6. Write end run an end-to-end test that
       1. creates a test copy.
       2. localizes it using this software and
---
+++ test/migrate/test_e2e.py
@@ -0,0 +1,53 @@
+import os
+import shutil
+import unittest
+
+from hamcrest import assert_that, contains_string
+
+from migrater import migrate
+
+IMPORTED_GRAPH_NAME = 'test-vault'
+IMPORTED_VAULT = os.path.join('test/data/test/', IMPORTED_GRAPH_NAME)
+TEST_GRAPH_DIRECTORY = 'test/data/graphs/'
+TEST_GRAPH = os.path.join(TEST_GRAPH_DIRECTORY, IMPORTED_GRAPH_NAME)
+TEST_PAGES = os.path.join(TEST_GRAPH, 'pages')
+TEST_ASSETS = os.path.join(TEST_GRAPH, 'assets')
+
+PATH1 = os.path.join(TEST_PAGES, 'page with some text.md')
+IMAGE = os.path.join(TEST_ASSETS, 'autocode.png')
+PATH2 = os.path.join(TEST_PAGES, 'another with an mp4.md')
+MP4   = os.path.join(TEST_ASSETS, 'adc.mp4')
+PATH3 = os.path.join(TEST_PAGES, 'page with a pdf.md')
+PDF   = os.path.join(TEST_ASSETS, 'README.pdf')
+
+def read(path):
+    with open(path) as f:
+        return f.read()
+
+
+class E2ETestCase(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls) -> None:
+        if os.path.exists(TEST_GRAPH):
+            shutil.rmtree(TEST_GRAPH)
+        shutil.copytree(IMPORTED_VAULT, TEST_GRAPH)
+        migrate(TEST_GRAPH)
+
+    def test_copies_image(self):
+        md = read(PATH1)
+        assert_that(md, contains_string('../assets/autocode.png'))
+        assert_that(os.path.exists(IMAGE))
+
+    def test_copies_mp4(self):
+        md = read(PATH2)
+        assert_that(md, contains_string('../assets/adc.mp4'))
+        assert_that(os.path.exists(MP4))
+
+    def test_copies_pdf(self):
+        md = read(PATH3)
+        assert_that(md, contains_string('../assets/README.pdf'))
+        assert_that(os.path.exists(PDF))
+
+
+if __name__ == '__main__':
+    unittest.main()
